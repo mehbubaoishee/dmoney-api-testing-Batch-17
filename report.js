a2122be46aa284ff56f4b4d8ebc6d2cd19b501ad
oisheemehbuba <oisheemehbuba@gmail.com>
2025-12-03 12:45:33 +0600

    project upload

---
+++ report.js
@@ -0,0 +1,17 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    collection:`https://api.postman.com/collections/44302140-4b85ba2b-8efb-42f1-b42d-00c714210df3?access_key=${process.env.pmatkey}`,
+    //collection:require('./collection/dmoney.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html',
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
