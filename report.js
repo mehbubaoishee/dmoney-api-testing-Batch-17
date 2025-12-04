103b8af61e9df92d21e029892541bf77ad822d2d
oisheemehbuba <oisheemehbuba@gmail.com>
2025-12-04 17:45:32 +0600

    Update

--- report.js
+++ report.js
@@ -2,6 +2,7 @@ const newman = require('newman');
 require('dotenv').config();
  
 newman.run({
+    //collection:`https://api.postman.com/collections/44302140-4b85ba2b-8efb-42f1-b42d-00c714210df3?access_key=${process.env.pmatkey}`,
     collection:`https://api.postman.com/collections/44302140-4b85ba2b-8efb-42f1-b42d-00c714210df3?access_key=${process.env.pmatkey}`,
     //collection:require('./collection/dmoney.json'),
     reporters: 'htmlextra',
